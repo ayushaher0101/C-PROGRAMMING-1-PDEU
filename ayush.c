73ca1b7c1316b6c048e9d7835b15b358570a3085
Ayush Aher <ayushnaher101@gmail.com>
2025-08-12 10:11:36 +0530

    Add files via upload

---
+++ ayush.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+int x,y;
+
+
+printf("enter the first no:");
+scanf("%d",&x);
+
+
+printf("enter the second no:");
+scanf("%d",&y);
+
+
+printf("the sum of the two numbers is %d",x+y);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
