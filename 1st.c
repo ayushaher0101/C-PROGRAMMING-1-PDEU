78ece959ee5ef25ee9007e619afed34412787996
Ayush Aher <ayushnaher101@gmail.com>
2025-08-26 10:13:22 +0530

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void main()
+{
+ int j,i=1,k;
+ printf("first 10 natural numbers:\n");
+
+ do
+ {
+     printf("%d\n",i);
+     i++;
+
+
+ }while(i<11);
+
+
+
+ k=1;
+printf("first 10 natural numbers are \n");
+
+while(k<11)
+
+{
+
+printf("%d\n",k);
+k=k+1;
+
+}
+
+
+
+printf("first 10 natural no\n");
+
+for (j=1;j<11;j=j+1)
+{
+    printf("%d\n",j);
+
+
+
+
+}
+}
