340d92bbecd6eaf9aabba1adfbcab4b88bbae7a5
Ayush Aher <ayushnaher101@gmail.com>
2025-09-13 09:47:13 +0530

    Add files via upload

---
+++ hours into minutes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+   float a;
+
+   printf("enter the no of hours:");
+   scanf("%f",&a);
+
+   printf("the hours converted into minutes are:%.2f",(a*60));
+
+
+
+
+}
