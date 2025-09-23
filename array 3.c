304f44954e1e79b0443b9dc942a19afeb53cee45
Ayush Aher <ayushnaher101@gmail.com>
2025-09-23 09:39:21 +0530

    Add files via upload

---
+++ array 3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n,temp,a[5];
+    
+    printf("enter the 5 numbers:");
+     for(i=0;i<5;i++)
+     {
+         scanf("%d",&a[i]);
+     }
+     for(i=0;i<5;i++)
+     for(j=0;j<5-i;j++)
+     {
+         if(a[j]>a[j+1])
+         {   
+             temp=a[j];
+             a[j]=a[j+1];
+             a[j+1]=temp;
+         }  
+     }
+     printf("sorted array is :");
+     for(i=0;i<5;i++)
+     printf("%d\n",a[i]);
+}
+
